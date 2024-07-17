5155e87558b125040c464bb1003f88fa60fbe340
Dmitry Kazantsev <dk@roboticket.com>
2024-07-17 13:00:29 +0200

    feat: cors

--- src/global/api/orders.api.ts
+++ src/global/api/orders.api.ts
@@ -3,7 +3,13 @@ import { baseApi } from "./base.api";
 export const ordersApi = {
   getOrders: () => {
     return baseApi
-      .get("orders/all")
+      .get("orders/all", {
+        headers: {
+          "Content-Type": "application/json",
+          "Allow-Control-Allow-Origin":
+            "https://ortvest-website-backend.vercel.app/",
+        },
+      })
       .then((response) => console.log("response", response));
   },
 };
