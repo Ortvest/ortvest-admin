db70ca6e470f53c9b7a2b0fceaeddfce7895a9cf
Dmitry Kazantsev <dk@roboticket.com>
2024-07-17 13:35:46 +0200

    feat: cors

--- src/global/api/base.api.ts
+++ src/global/api/base.api.ts
@@ -2,14 +2,12 @@ import axios from "axios";
 
 const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 const backendUrl = import.meta.env.VITE_BACKEND_APP;
+
 export const baseApi = axios.create({
   baseURL: backendUrl,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${accessToken}`,
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://ortvest-website-backend.vercel.app",
-    "Access-Control-Allow-Credentials": "true",
-    mode: "cors",
   },
 });
