c90b6c899e6d3c90fdc8d116af0736697df9a1a6
Dmitry Kazantsev <dk@roboticket.com>
2024-07-17 11:55:41 +0200

    feat: cors

--- src/global/api/base.api.ts
+++ src/global/api/base.api.ts
@@ -4,6 +4,7 @@ const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 const backendUrl = import.meta.env.VITE_BACKEND_APP;
 export const baseApi = axios.create({
   baseURL: backendUrl,
+  withCredentials: true,
   headers: {
     Authorization: `Bearer ${accessToken}`,
     "Content-Type": "application/json",
